675a549a7fd6b2879dbfa82c44b527dbab958012
ammarfakhar123 <ammarfakhar123@gmail.com>
2026-05-14 14:53:52 +0500

    Add

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,24 @@
+import Image from "next/image";
 import React from "react";
+import {navLinks} from "./../data/Data"
+import Link from "next/link";
+import { FiPhone } from "react-icons/fi";
+import { FaRegUserCircle } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="bg-transparent py-6 px-6 flex items-center justify-between">
+      <Image src="/logo.png" alt="img" width={130} height={22} priority />
+      <div className="flex items-center gap-6 text-white">
+        {navLinks.map((nav) => (
+          <Link href={nav.link} className="text-sm ">{nav.title}</Link>
+        ))}
+      </div>
+      <div className="flex items-center gap-6">
+        <span className="flex items-center gap-2 text-white text-sm"><FiPhone /> +92 320 9430934</span>
+        <span className="flex items-center gap-2 text-white text-sm"><FaRegUserCircle /></span>
+        <button className="border bg-transparent cursor-pointer text-white w-[128px] h-[42px] rounded-full text-[15px] ">Add Property</button>
+      </div>
     </div>
   );
 };
---
+++ app/data/Data.js
@@ -0,0 +1,8 @@
+export const navLinks = [
+  { id: 0, title: "Home", link: "/" },
+  { id: 1, title: "Listing", link: "/listing" },
+  { id: 2, title: "Members", link: "/members" },
+  { id: 3, title: "Blog", link: "/blog" },
+  { id: 4, title: "Pages", link: "/pages" },
+  { id: 5, title: "Contact", link: "/contact" },
+];
